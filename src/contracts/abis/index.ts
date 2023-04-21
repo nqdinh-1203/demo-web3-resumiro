25129f440b28c2647df7d3bb4ee4f425edee264f
nqdinh-1203 <quangdinhnt1203@gmail.com>
2023-04-22 04:33:56 +0700

    add address remaining contract and fix resume abi

--- src/contracts/abis/index.ts
+++ src/contracts/abis/index.ts
@@ -8,10 +8,10 @@ import userAbi from "./User.json";
 
 export const certificateAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const companyAddress = "0xC036BdbC5eF4050FC224bBAC7EBf84bDF1b899e7";
-export const experienceAddress = ""; // done
-export const jobAddress = ""; // 
-export const resumeAddress = ""; // done
-export const skillAddress = ""; // done
+export const experienceAddress = "0x360446a9f6fCEeda3Cca4a307206297bBfda92d5"; // done
+export const jobAddress = "0xc72E822Facbf3EB2423D2EB7D46DC0ae4e3AC616"; // done
+export const resumeAddress = "0x226d38ee05e515683a81bFa2375566E9c50B001c"; // done
+export const skillAddress = "0x2941d0e1DA4E4534bCFAA5170BA009160F62523b"; // done
 export const userAddress = "0x8ae8570C43C8774c73b3c9Fc0D21363FaE38b56D";
 
 export { certificateAbi, companyAbi, experienceAbi, jobAbi, resumeAbi, skillAbi, userAbi };
